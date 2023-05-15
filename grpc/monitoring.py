e7fcd4929e84307ea1a83cbc6ccce3328b6905bb
svelezp <svelezp10@eafit.edu.co>
2023-05-15 17:43:07 -0500

    grpc
    avance del grpc

---
+++ grpc/monitoring.py
@@ -0,0 +1,10 @@
+import grpc
+
+
+class monitoreo():
+    def vigilancia():
+        float = i
+        i = 0
+        while True():
+            i = i + 0.20
+            #Mandar esto constantemente
